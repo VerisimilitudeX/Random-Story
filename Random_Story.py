6c7ecb73b349146d354d646bbda498138350359f
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-19 17:47:00 -0800

    Update Random_Story.py

--- Random_Story.py
+++ Random_Story.py
@@ -1,4 +1,5 @@
 import random
+
 when = ['A few years ago', 'Yesterday', 'Last night', 'A long time ago','On the 20th of January']
 who = ['a rabbit', 'an elephant', 'a mouse', 'a turtle','a cat']
 name = ['Jeff', 'Bob','Adam', 'Mrs. Isitt', 'Piyush']
@@ -6,6 +7,7 @@ residence = ['Barcelona','India', 'Germany', 'Venice', 'England']
 went = ['a movie', 'a university','a seminar', 'a school', 'to an underground palace']
 happened = ['made a lot of friends','ate a burger', 'found a secret key', 'solved a mystery', 'wrote a book']
 x=1
+
 while x <= 5:
     x += 1
     print(random.choice(when) + ', ' + random.choice(who) + ' who lived in ' + random.choice(residence) + ', went to ' + random.choice(went) + ', and ' + random.choice(happened) + '!')
